4fa398dcc47ea42bbc67029976e56af9a7f0303a
screenviolence <demidov.03.md@gmail.com>
2025-05-17 00:57:30 +0700

    feat: enhance CORS settings, email configuration, and add full name filtering in student queries

--- CoachDiary_Backend/settings/cors.py
+++ CoachDiary_Backend/settings/cors.py
@@ -2,8 +2,7 @@ from django.conf import settings
 
 CORS_ALLOWED_ORIGINS = ["https://coachdiary.ru", "http://127.0.0.1:8000"]
 CORS_ALLOW_CREDENTIALS = True
+SECURE_SSL_REDIRECT = False
 
-if settings.DEBUG:
-    SECURE_SSL_REDIRECT = False
-else:
+if not settings.DEBUG:
     SECURE_SSL_REDIRECT = True
--- CoachDiary_Backend/settings/general.py
+++ CoachDiary_Backend/settings/general.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "10.0.2.2", "coachdiary.ru"]
 
