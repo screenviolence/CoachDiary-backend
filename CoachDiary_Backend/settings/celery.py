86a04b118f989dc0decd85624df2526625e277fc
screenviolence <demidov.03.md@gmail.com>
2025-06-02 18:51:31 +0700

    feat: configure Docker setup with PostgreSQL and Redis for improved async task handling

--- CoachDiary_Backend/settings/celery.py
+++ CoachDiary_Backend/settings/celery.py
@@ -3,8 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER_URL",
+    f"redis://{os.getenv('REDIS_HOST', 'localhost')}:6379/0",
+)
+
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_RESULT_BACKEND",
+    f"redis://{os.getenv('REDIS_HOST', 'localhost')}:6379/1",
+)
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
