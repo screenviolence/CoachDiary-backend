c34f56e7e82b8959b35a6f1b293e8d674e297b61
screenviolence <demidov.03.md@gmail.com>
2025-06-08 00:07:19 +0700

    feat: implement signals for automatic StudentStandard creation and enhance model validation

--- students/signals.py
+++ students/signals.py
@@ -1,9 +1,10 @@
 import uuid
 
-from django.db.models.signals import post_save
+from django.db.models import Count
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from students.models import Student, Invitation
+from students.models import Student, Invitation, StudentClass
 
 
 @receiver(post_save, sender=Student)
@@ -17,3 +18,16 @@ def create_invitation(sender, instance, created, **kwargs):
                 invite_code=invite_code,
                 is_used=False
             )
+
+
+@receiver(post_delete, sender=Student)
+@receiver(post_save, sender=Student)
+def clean_empty_classes(sender, instance, **kwargs):
+    """
+    Находит и удаляет все пустые классы после изменений студентов.
+    """
+    empty_classes = StudentClass.objects.annotate(
+        student_count=Count('students')
+    ).filter(student_count=0)
+
+    empty_classes.delete()
